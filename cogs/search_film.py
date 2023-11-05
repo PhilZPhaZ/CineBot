f505c30c9e1598756516c15030cbcd7dce48714a
PhilZPhaZ <bour.phileas@gmail.com>
2023-11-05 22:45:04 +0100

    added info command

--- cogs/search_film.py
+++ cogs/search_film.py
@@ -46,10 +46,36 @@ class SearchMusic(commands.Cog):
         """
         self.result = self.movie.search(nom_du_film)
         top_10_results = list(self.result.results)[:10]
-        emb = discord.Embed(title="Resultats")
+        emb = discord.Embed(title="Resultats - 10 films les plus populaires", color=discord.Color.from_rgb(69, 44, 129))
         for i, res in enumerate(top_10_results):
             emb.add_field(name=f"{i+1} - {res.title}", value=f"[Poster de : {res.title}](https://image.tmdb.org/t/p/w500{res.poster_path})", inline=False)
         await interaction.response.send_message(embed=emb)
+    
+    @app_commands.command()
+    async def info(self, interaction, nom_du_film: str):
+        """Search for movies and display the top 10 results with their posters.
+
+        Args:
+            interaction: The interaction object.
+            nom_du_film: The name of the movie to search for.
+
+        Returns:
+            None
+
+        """
+        self.result = self.movie.search(nom_du_film)
+        top_movie = list(self.result.results)[0]
+        emb = discord.Embed(title=f"{top_movie.title}", color=discord.Color.from_rgb(69, 44, 129))
+        emb.set_thumbnail(url=f"https://image.tmdb.org/t/p/w500{top_movie.poster_path}")
+        emb.add_field(name="Date de sortie", value=f"{top_movie.release_date}", inline=False)
+
+        len_overview_movie = len(top_movie.overview)
+        if len_overview_movie > 1900:
+            emb.add_field(name="Synopsis", value=f"{top_movie.overview[:1900]}...", inline=False)
+        else:
+            emb.add_field(name="Synopsis", value=f"{top_movie.overview}", inline=False)
+        await interaction.response.send_message(embed=emb)
+        
 
 async def setup(bot):
     await bot.add_cog(SearchMusic(bot))
